9172859f21cb6b3fd84a08bde46c00ec85bdac02
mahmoudgamalmmg8 <mahmoudgamalmmg8@gmail.com>
2020-11-19 16:10:53 +0200

    add job moudle

--- job/models.py
+++ job/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+JOB_TYPE=(
+    ("full time","full time"),
+    ("half time","half time"),
+)
 # Create your models here.
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE)
+    description= models.TextField(max_length=1000)
+    published_At = models.DateTimeField(auto_now=True, auto_now_add=False)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience= models.IntegerField(default=1)
+    def __str__(self):
+        return self.title,self.published_At
+    
