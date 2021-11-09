8917dfd80727c0e585203b57dd4764c81c7de135
mahmoudgamalmmg8 <44068022+mahmoudgamalmmg8@users.noreply.github.com>
2021-11-09 22:43:18 +0200

    add job views and urls

--- job/models.py
+++ job/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
 
 # Create your models here.
@@ -16,7 +17,12 @@ class job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-    
+    category = models.ForeignKey('category',on_delete=CASCADE)
 
     def __str__(self):
         return self.title
+
+class category(models.Model):
+    name = CharField(max_length=25)
+    def __str__(self) -> str:
+        return self.name
